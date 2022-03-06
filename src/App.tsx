68c815e32dc3a0f8c68c4ca6a2ae073a0396f9bb
jhon192 <jhon19.2@outlook.com>
2022-03-05 22:12:41 -0400

    components updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Contacts from "./components/Contacts";
 import Schedule from "./components/Schedule";
 import { ContactDatas } from "./interfaces/ContactDatas";
 
-
-
 const App = (): JSX.Element => {
+  const [Contactdata, setContactdata] = useState<ContactDatas[]>([
+    {
+      name: "jhon",
+      lastname: "garcia",
+      number: 1234567890,
+    },
+  ]);
 
-  const [Contactdata, setContactdata] = useState<ContactDatas[]>([]);
+  const getContactdata = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setContactdata([ ...Contactdata, { [event.target.name]: event.target.value } ])
+  };
+
+  const getClick = ( event: React.MouseEvent<HTMLButtonElement> ) => {
+    event.preventDefault()
+
+    console.log(Contactdata);
+
+  }
 
   return (
     <div className="w-100 h-100 d-flex">
-      <Schedule />
-      <Contacts Contacts={Contactdata}/>
+      <Schedule getContactdata={getContactdata} getClick={getClick} />
+      <Contacts Contacts={Contactdata} />
     </div>
   );
 };
--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 
-const Schedule = () => {
+interface props {
+  getContactdata(event: React.ChangeEvent<HTMLInputElement>): void
+  getClick(event: React.MouseEvent<HTMLButtonElement>): void
+}
+
+const Schedule = ({ getContactdata,  getClick}: props) => {
   return (
     <div className="h-100 w-100 d-flex align-items-center justify-content-center">
       <div className="card">
         <div className="card-body">
-          <form className="">
+          <form>
             <div className="m-1 p-1 form-floating">
-              <input type="text" className="form-control" placeholder="Name" />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                name="name"
+                onChange={getContactdata}
+              />
               <label className="form-label">Name</label>
             </div>
             <div className="m-1 p-1 form-floating">
@@ -15,6 +26,8 @@ const Schedule = () => {
                 type="text"
                 className="form-control"
                 placeholder="lastname"
+                name="lastname"
+                onChange={getContactdata}
               />
               <label className="form-label">Lastname</label>
             </div>
@@ -23,15 +36,15 @@ const Schedule = () => {
                 type="number"
                 className="form-control"
                 placeholder="Phone number"
+                name="number"
+                onChange={getContactdata}
               />
               <label className="form-label">Phone number</label>
             </div>
             <div className="m-1 p-1 text-center">
-              <input
-                type="submit"
-                className="btn btn-success"
-                value="Send info"
-              />
+              <button className="btn btn-success" onClick={getClick}>
+                send info
+              </button>
             </div>
           </form>
         </div>
